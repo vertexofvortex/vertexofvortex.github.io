90561a4f152dc95b2c4f7235cc57979e7b00a66d
vertexofvortex <tbm92ru@gmail.com>
2020-04-26 01:03:22 +0300

    forms linked

--- imggen/js/canvas.js
+++ imggen/js/canvas.js
@@ -7,4 +7,20 @@ function generateCanvas() {
     var url = link.value;
     var canvas = document.getElementById("capture");
     canvas.style.backgroundImage = "url(" + url + ")";
+
+    var heading, selectorHead, selectorHeadPick;
+    for (var i = 1; i < 5; i++) {
+        selectorHead = "#heading" + i;
+        selectorHeadPick = "#heading" + i + "Pick";
+        heading = document.querySelector(selectorHeadPick);
+        document.querySelector(selectorHead).innerHTML = heading.value;
+    }
+
+    var text, selectorText, selectorTextPick;
+    for (var i = 1; i < 5; i++) {
+        selectorText = "#desc" + i;
+        selectorTextPick = "#desc" + i + "Pick";
+        text = document.querySelector(selectorTextPick);
+        document.querySelector(selectorText).innerHTML = text.value;
+    }
 }
