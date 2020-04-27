7da28b7fd013a5be33ed4857f8ad72d3260f0c11
vertexofvortex <tbm92ru@gmail.com>
2020-04-27 13:31:33 +0300

    icon uploading fix

--- imggen/js/canvas.js
+++ imggen/js/canvas.js
@@ -1,16 +1,21 @@
 document.getElementById("generate").onclick = fillCanvas;
 document.getElementById("create").onclick = createCanvas;
 document.getElementById("download").onclick = downloadCanvas;
-
+var placeholder = document.getElementById("previewPlaceholder");
 function createCanvas() {
     var scale = document.getElementById("scalePick").value;
     var result = document.querySelector(".output");
     var infoline = document.querySelectorAll(".infoline")[1];
     html2canvas(document.querySelector("#capture"), {scale: scale, useCORS: true,}).then(picOutput => {
         result.innerHTML = "";
-        result.appendChild(infoline);
         result.appendChild(picOutput);
         picOutput.id = "canvas";
+        if (document.querySelector("#canvas[width=\"0\"]")) {
+            result.innerHTML = "";
+            window.placeholder.innerHTML = "<p>Ошибка. Вероятно, масштаб холста установлен в недопустимое значение.</p>";
+            result.appendChild(window.placeholder);
+        }
+        result.appendChild(infoline);
     });
 }
 function downloadCanvas() {
@@ -53,6 +58,7 @@ function fillCanvas() {
         document.querySelector(selectorText).innerHTML = text.value;
     }
     imgUpload();
+    iconToFA();
 }
 function imgUpload() {
     var index = 0;
@@ -62,8 +68,10 @@ function imgUpload() {
         let elemId = id.substr(7).toLowerCase();
         let element = document.getElementById(elemId);
         index++;
-
         let file = input.files[0];
+        if (file === undefined) {
+            continue;
+        }
         let reader = new FileReader();
         reader.readAsDataURL(file);
         reader.onload = function() {
@@ -72,17 +80,40 @@ function imgUpload() {
     } while (input != undefined);
 }
 
-function readURL(e) {
-    if (this.files && this.files[0]) {
-        var reader = new FileReader();
-        reader.load(function(e) { 
-            elemId.style.backgroundImage = e.target.result; 
-        });
-        reader.readAsDataURL(this.files[0]);
-    }
+function showMenu(title = "Без заголовка", width = 100, height = 200) {
+    alert(title + ' ' + width + ' ' + height);
 }
 
-function notify(text = "text is not added") {
-    var bar = document.querySelector(".notification");
-    bar.innerHTML = text;
+function sleep(milliseconds) {
+    const date = Date.now();
+    let currentDate = null;
+    do {
+      currentDate = Date.now();
+    } while (currentDate - date < milliseconds);
+}
+
+function notify(text = "notification") {
+    body = document.querySelector("body");
+    bar = document.createElement("div");
+    bar.innerHTML = "text";
+    body.insertAdjacentElement("afterbegin", bar)
+    bar.style.cssText = "height: 100px; transition: 1s ease-in-out";
+    sleep(2000);
+    bar.style.cssText = "display: none";
+}
+
+function iconToFA() {
+    var index = 0;
+    do {
+        var input = document.querySelectorAll("input.FAicon")[index];
+        let id = input.id;
+        var elemId = id.substr(6).toLowerCase();
+        var element = document.getElementById(elemId);
+        console.log(input, elemId);
+        index++;
+
+        let icon = document.createElement("i");
+        icon.className = "fas fa-" + input.value;
+        element.replaceWith(icon);
+    } while (input != undefined);
 }
