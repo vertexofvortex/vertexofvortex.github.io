5ac0dfd0acc74310411ea50a73ba080f562d727c
vertexofvortex <vertexofvortex@users.noreply.github.com>
2023-05-09 17:28:36 +0000

    deploy: fcb17185d07bb0d2731b09d054709d64b24aa0f4

--- assets/MainContent-71c6713c.js
+++
@@ -1,1 +0,0 @@
-import{j as n,f as s}from"./index-8eab7932.js";function o({children:t}){return n.jsx("main",{children:n.jsx("div",{className:`${s.content} content`,children:t})})}export{o as M};
---
+++ assets/projects-a02ceb50.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-951c822a.js";import{M as r}from"./MainContent-7baafcd0.js";function n(){return t.jsx(r,{children:"Projects"})}export{n as default};
