b117cbde7c120b65305f3850c7c5e71f4ccc8a83
vertexofvortex <tbm92ru@gmail.com>
2023-05-09 18:18:59 +0300

    Updates

---
+++ assets/MainContent-71c6713c.js
@@ -0,0 +1,1 @@
+import{j as n,f as s}from"./index-8eab7932.js";function o({children:t}){return n.jsx("main",{children:n.jsx("div",{className:`${s.content} content`,children:t})})}export{o as M};
---
+++ assets/projects-077fef11.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-8eab7932.js";import{M as r}from"./MainContent-71c6713c.js";function n(){return t.jsx(r,{children:"Projects"})}export{n as default};
