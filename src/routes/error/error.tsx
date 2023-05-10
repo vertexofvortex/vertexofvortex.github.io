335071a255f974e9dcc3126f6d91c9d521badf44
vertexofvortex <tbm92ru@gmail.com>
2023-05-10 19:58:55 +0300

    🥅 Error page is now showing a router error message

--- src/routes/error/error.tsx
+++ src/routes/error/error.tsx
@@ -1,11 +1,16 @@
+import { useRouteError } from "react-router-dom";
 import { MainContent } from "../../components";
+import s from "./error.module.scss";
 
 export function ErrorPage() {
+  const error = useRouteError();
+
   return (
     <MainContent>
       <div className="cmd">
         <span style={{ color: "var(--vim-vimCommand)" }}>
           {`\ue654`} An error occured. Look into the browser console
+          <div className={s.errorText}>{JSON.stringify(error)}</div>
         </span>
       </div>
     </MainContent>
