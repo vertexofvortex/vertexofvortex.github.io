9429135dc7487190c29af40abce9f0dae7a51484
vertexofvortex <tbm92ru@gmail.com>
2023-05-13 21:07:53 +0300

    🩹 Some UI fixes

--- src/components/ProjectsBlock/ProjectsBlock.tsx
+++ src/components/ProjectsBlock/ProjectsBlock.tsx
@@ -11,7 +11,7 @@ export function ProjectsBlock({ data }: Props) {
   return (
     <Section className={s.projects} promptCommand="ls ./projects -U | head -2">
       <div className={s.content}>
-        {data.map((project, key) => (
+        {data.slice(data.length - 2, data.length).map((project, key) => (
           <ProjectCard {...project} key={key} />
         ))}
       </div>
@@ -25,42 +25,3 @@ export function ProjectsBlock({ data }: Props) {
 }
 
 export default ProjectsBlock;
-
-{
-  /* <ProjectCard
-      pictureUrl="/pictures/mock.jpg"
-      title="VK RSS Parser"
-      description="Сервис, который позволяет подключить группы со своего аккаунта ВКонтакте и привязать к ним источники RSS-лент, с которых будут опрашиваться новости. Интересующие вас посты затем можно выбрать, отредактировать и опубликовать в группе вместе с красивым сгенерированным сниппетом-картинкой"
-      buttons={[
-        {
-          className: "button-link blue",
-          href: "",
-          text: "\udb80\udea2 Репозиторий с исходным кодом",
-        },
-      ]}
-    />
-    <ProjectCard
-      pictureUrl="/pictures/mock.jpg"
-      title="Digital Assets Capital"
-      description="Приватный инвестиционный фонд, в разработке которого я принимал далеко не самое последнее участие"
-      buttons={[
-        {
-          className: "button-link blue",
-          href: "",
-          text: "\udb81\udd9f Перейти на сайт",
-        },
-      ]}
-    />
-    <ProjectCard
-      pictureUrl="/pictures/mock.jpg"
-      title="Project Armor"
-      description="Польская киберспортивная организация, имеет свой состав по различным играм, а также занимается проектами в сфере криптоиндустрии и NFT. Я занимался разработкой их веб-сайта"
-      buttons={[
-        {
-          className: "button-link blue",
-          href: "https://project-armor.io/main",
-          text: "\udb81\udd9f Перейти на сайт",
-        },
-      ]}
-    /> */
-}
