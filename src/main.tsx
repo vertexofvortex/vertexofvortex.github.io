e41e1cd8310c93fe0610bb59f9b79535a38ccc3e
vertexofvortex <tbm92ru@gmail.com>
2023-05-14 19:56:26 +0300

    🐛 Fixed fonts

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.scss";
+import "./fonts.css";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router";
 
